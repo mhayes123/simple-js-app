c0996fdece97a607a2f4381f75a0fe5e03a071a5
mhayes123 <mhayes30@comcast.net>
2022-06-13 23:17:12 -0700

    Added Pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList=[
+  {name: 'Pikachu', height: '14.4', type: 'Electricity'},
+  {name: 'Squirtle', height: '21.6', type: 'Water'},
+  {name: 'Charmander', height: '24.0', type: 'fire'},
+  {name: 'Pidgey', height: '12.0', type: ['Normal', 'Flying']},];
