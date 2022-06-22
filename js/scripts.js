7b26c415075ec67588057a1a73eb2ff3461182fa
mhayes123 <mhayes30@comcast.net>
2022-06-21 17:51:28 -0700

    added document write into for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 let pokemonList=[
-  {name: 'Pikachu', height: '14.4', type: 'Electricity'},
-  {name: 'Squirtle', height: '21.6', type: 'Water'},
-  {name: 'Charmander', height: '24.0', type: 'fire'},
-  {name: 'Pidgey', height: '12.0', type: ['Normal', 'Flying']},];
+  {name: 'Pikachu', height: '4', type: 'Electricity'},
+  {name: 'Gyarados', height: '21.4', type: ['Water','flying']},
+  {name: 'Charmander', height: '6', type: 'fire'},
+  {name: 'Pidgey', height: '1.5', type: ['Normal', 'Flying']}
+]
+
+  //Used document.write() inside the loop’s code to write the Pokémon name on my website DOM.
+for (let i = 0; i < pokemonList.length; i++){
+    if(pokemonList[i].height >20){
+    document.write("Name: " + pokemonList[i].name + " Height: " + pokemonList[i].height + " Wow, thats a big Pokemon! <br> ");
+}   else if(pokemonList[i].height <3){
+    document.write("Name:  " + pokemonList[i].name + " Height: " + pokemonList[i].height + " small Pokemon <br>");
+}   else
+    document.write("Name: " + pokemonList[i].name + " Height: " + pokemonList[i].height + " normal sized Pokemon <br>");
+}
+
+
