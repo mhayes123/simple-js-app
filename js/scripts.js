f797fb640fa941e5bc48982d3a7808bae2caeaa0
mhayes123 <mhayes30@comcast.net>
2022-06-30 21:50:25 -0700

    Re wrote file, pokemon, and included IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,38 @@
-let pokemonRepository = (function () {
-    
-  let pokemonList = [ 
-    {name: 'Pikachu',   height: '4', type: 'Electricity'},
-    {name: 'Gyarados', height: '21.4', type: ['Water','flying']},
-    {name: 'Charmander', height: '6', type: 'fire'},
-    {name: 'Pidgey', height: '1.5', type: ['Normal', 'Flying']}
-  ];
+let pokemonRepository = (function (){
+    let pokemonList =  
+      
+    let pokeCharmander = {
+        name: 'Charmander',
+        type: 'Fire',
+        height: 2
+      };
+      
+    let pokeBeedrill = {
+        name: 'Beedrill',
+        type: ['Bug', 'Poison'],
+        height: 3
+      };
+      
+    let pokePikachu = {
+        name: 'Pikachu',
+        type: ['Electric'],
+        height: 1,
+      };
+      
+    let pokeDragonite = {
+        name: 'Dragonite',
+        type: ['Dragon', 'Flying'],
+        height: 7,
+      };
 
-  pokemonList.forEach(function(user) {
-    console.log(user.name + '  ' + user.height + '  ' + user.type + '  ' );
-  });
+    let pokemonRepository = (function () { //IIFE
+      let pokemonList = [
+        pokeCharmander,
+        pokeBeedrill,
+        pokePikachu,
+        pokeDragonite,
+        
+      ];
 
     function add(pokemon) {
       pokemonList.push(pokemon);
@@ -25,5 +48,19 @@ let pokemonRepository = (function () {
     };
   })();
 
+  function loopFunction(pokemons) {
+    document.write(pokemons.name);
+    document.write(' (' + 'height: ' + pokemons.height + ') ');
+    if (pokemons.height > 5) {
+      document.write(' - WOW, this is a big pokemon!');
+    }else if (pokemons.height <= 5 && pokemons.height >= 3) {
+      document.write('This is an average pokemon');
+    }else {
+      document.write('This is a Small pokemon');
+    }
+    document.write('<br>');
+  }
+  
+  pokemonRepository.add(pokeDragonite);
+  pokemonRepository.getAll().forEach(loopFunction);})
   console.log(pokemonRepository.getAll)()
-
