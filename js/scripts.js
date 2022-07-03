b73de1418306f2a4f63e35c1b6bfe1d2dcc6d185
mhayes123 <mhayes30@comcast.net>
2022-07-02 19:09:01 -0700

    Made structure/layout changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,52 @@
-let pokemonRepository = (function (){
-    let pokemonList =  
-      
-    let pokeCharmander = {
-        name: 'Charmander',
-        type: 'Fire',
-        height: 2
-      };
-      
-    let pokeBeedrill = {
-        name: 'Beedrill',
-        type: ['Bug', 'Poison'],
-        height: 3
-      };
-      
-    let pokePikachu = {
-        name: 'Pikachu',
-        type: ['Electric'],
-        height: 1,
-      };
-      
-    let pokeDragonite = {
-        name: 'Dragonite',
-        type: ['Dragon', 'Flying'],
-        height: 7,
-      };
+let pokemonRepository = (function () {
+  //IIFE
+  let pokemonList = [
+    {
+      name: "Charmander",
+      type: "Fire",
+      height: 2,
+    },
+    {
+      name: "Beedrill",
+      type: ["Bug", "Poison"],
+      height: 3,
+    },
+    {
+      name: "Pikachu",
+      type: ["Electric"],
+      height: 1,
+    },
+    {
+      name: "Dragonite",
+      type: ["Dragon", "Flying"],
+      height: 7,
+    },
+  ];
 
-    let pokemonRepository = (function () { //IIFE
-      let pokemonList = [
-        pokeCharmander,
-        pokeBeedrill,
-        pokePikachu,
-        pokeDragonite,
-        
-      ];
-
-    function add(pokemon) {
-      pokemonList.push(pokemon);
-    }
-  
-    function getAll() {
-      return pokemonList;
-    }
-  
-    return {
-      add: add,
-      getAll: getAll
-    };
-  })();
-
-  function loopFunction(pokemons) {
-    document.write(pokemons.name);
-    document.write(' (' + 'height: ' + pokemons.height + ') ');
-    if (pokemons.height > 5) {
-      document.write(' - WOW, this is a big pokemon!');
-    }else if (pokemons.height <= 5 && pokemons.height >= 3) {
-      document.write('This is an average pokemon');
-    }else {
-      document.write('This is a Small pokemon');
-    }
-    document.write('<br>');
+  function add(pokemon) {
+    pokemonList.push(pokemon);
   }
-  
-  pokemonRepository.add(pokeDragonite);
-  pokemonRepository.getAll().forEach(loopFunction);})
-  console.log(pokemonRepository.getAll)()
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+function loopFunction(pokemon) {
+  document.write(pokemon.name);
+  document.write(" (" + "height: " + pokemon.height + ") ");
+  if (pokemon.height > 5) {
+    document.write(" - WOW, this is a big pokemon!");
+  } else if (pokemon.height <= 5 && pokemon.height >= 3) {
+    document.write("This is an average pokemon");
+  } else {
+    document.write("This is a Small pokemon");
+  }
+  document.write("<br>");
+}
+pokemonRepository.getAll().forEach(loopFunction);
